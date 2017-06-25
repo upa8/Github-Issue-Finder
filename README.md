e85fa3948e1a5eaa1a3b991328023249b0bbc900
Pratik Upacharya <pratikupacharya@gmail.com>
2017-06-25 19:50:01 +0530

    Basic app created which changes text on button click

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # Github-Issue-Finder
 A React-Native application which finds issues of a particular Github Repository
+
+
+Subtasks -
+
+*) Install nodejs - Done
+*) Install react-native - Done
+*) Install exponent - Done
+*) Run first basic app - Done
+*) Setup github repository - Done
+*) Learn react-native from official documentation - In progress
+*) Create a nodejs server project
+*) Change png files for the project
+*) Build android apk and run it on mobile device
+*) Read about the github api - In progress
+*) Installed genymotion
+*) Installed virtual box
+*) Watch tutorials on flex basics and layout
+*) Read about redux, redux patterns, 
+
+
--- main.js
+++ main.js
@@ -1,24 +1,53 @@
 import Expo from 'expo';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, Alert } from 'react-native';
+
+
 
 class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Are you able to understand what I am creating?</Text>
-      </View>
-    );
-  }
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+          something: "Something this is nothing but a bug"
+        };
+        this.onPressLearnMore = this.onPressLearnMore.bind(this);
+    }
+
+
+    onPressLearnMore(){
+        Alert.alert('You tapped the button!');
+        this.setState({something: "Hello"});
+    }
+    render() {
+        return (
+            <View style={styles.container}>
+                
+                <Text> Hi {this.state.something}</Text>
+                
+                <TextInput style={{height: 40, borderColor: 'gray', width:300, borderWidth: 1}}/>
+
+                <Button
+                    onPress={this.onPressLearnMore}
+                    title="Click here"
+                    color="#841584"
+                    accessibilityLabel="Learn more about this purple button" />
+
+            </View>
+        );
+    }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
 
-Expo.registerRootComponent(App);
+
+    const styles = StyleSheet.create({
+        container: {
+            flex: 1,
+            backgroundColor: '#fff',
+            alignItems: 'center',
+            justifyContent: 'center',
+        },
+    });
+
+    Expo.registerRootComponent(App);
