70ed76e2297b2eaa00f55c42bf9596f9a8e0a77a
Pratik Upacharya <pratikupacharya@gmail.com>
2017-06-26 07:46:44 +0530

    Implemented extracting text from TextInput and Github API

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 import Expo from 'expo';
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, TextInput, Button, Alert } from 'react-native';
-
-
+import { StyleSheet, Text, View, TextInput, Button, Alert, FlatList } from 'react-native';
+import axios from 'axios';
 
 class App extends React.Component {
 
@@ -10,23 +9,34 @@ class App extends React.Component {
         super(props);
 
         this.state = {
-          something: "Something this is nothing but a bug"
+          something: "Something this is nothing but a bug",
+          issueList: [],
+          searchText: ''
         };
         this.onPressLearnMore = this.onPressLearnMore.bind(this);
     }
 
 
     onPressLearnMore(){
-        Alert.alert('You tapped the button!');
-        this.setState({something: "Hello"});
+        console.log('Search text ' + this.state.searchText);
+        axios.get(`https://api.github.com/repos/facebook/react-native/issues?page=1&per_page=10`)
+        .then(res => {
+        debugger
+        const issues = res.data;
+        this.setState({ issueList : issues });
+      });
     }
+
     render() {
         return (
             <View style={styles.container}>
                 
-                <Text> Hi {this.state.something}</Text>
+                <Text> Hi {this.state.searchText}</Text>
                 
-                <TextInput style={{height: 40, borderColor: 'gray', width:300, borderWidth: 1}}/>
+                <TextInput 
+                    style={{height: 40, borderColor: 'gray', width:300, borderWidth: 1}}
+                    onChangeText={(text) => this.setState({searchText:text})}
+                />
 
                 <Button
                     onPress={this.onPressLearnMore}
@@ -34,20 +44,37 @@ class App extends React.Component {
                     color="#841584"
                     accessibilityLabel="Learn more about this purple button" />
 
+                     <FlatList
+                        data={[
+                          {key: 'Devin'},
+                          {key: 'Jackson'},
+                          {key: 'James'},
+                          {key: 'Joel'},
+                          {key: 'John'},
+                          {key: 'Jillian'},
+                          {key: 'Jimmy'},
+                          {key: 'Julie'},
+                        ]}
+                        renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+                    />
             </View>
         );
     }
 }
 
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 
-
-    const styles = StyleSheet.create({
-        container: {
-            flex: 1,
-            backgroundColor: '#fff',
-            alignItems: 'center',
-            justifyContent: 'center',
-        },
-    });
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+});
 
     Expo.registerRootComponent(App);
