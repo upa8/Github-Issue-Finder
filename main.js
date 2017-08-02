4009a99e4a2b9b37f264d8d3fd1b50429f6a2540
Pratik Upacharya <=pratikupacharya@gmail.com>
2017-08-02 16:32:09 +0530

    Added navigation buttons

--- main.js
+++ main.js
@@ -1,124 +1,190 @@
-  import Expo from 'expo';
-  import React, { Component } from 'react';
-  import { StyleSheet, Text, View, TextInput, Button, Alert, FlatList } from 'react-native';
-  import axios from 'axios';
+import Expo from 'expo';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, Alert, FlatList } from 'react-native';
+import axios from 'axios';
 
-  class App extends React.Component {
+class App extends React.Component {
 
-      constructor(props) {
-          super(props);
+    constructor(props) {
+        super(props);
 
-          this.state = {
+        this.state = {
             something: "Something this is nothing but a bug",
             issueList: [],
             searchText: '',
             loading: false,
-            loadingText: 'Loading'
-          };
-          this.onPressLearnMore = this.onPressLearnMore.bind(this);
-      }
+            loadingText: 'Loading',
+            nextUrl: '',
+            prevUrl: '',
+            lastUrl: '',
+            firstUrl: ''
+        };
+
+        // need to refactore this
+        this.onPressLearnMore = this.onPressLearnMore.bind(this);
+        this.next = this.next.bind(this);
+        this.previous = this.previous.bind(this);
+        this.first = this.first.bind(this);
+        this.last = this.last.bind(this);
+
+    }
 
 
-      onPressLearnMore(){
-          
-          var url = 'https://api.github.com/repos/'+ this.state.searchText +'/issues?page=1&per_page=10';
-          
-          // Empty issueList 
-          var emptyList = [];
-          this.setState({ issueList : emptyList });
-          // Set error message 
-          this.setState({ loading : true });
-          this.setState({ loadingText : 'Loading Please wait' });
-              
-          // set loading text   
-          axios.get(url)
+    onPressLearnMore(urlData, urlValue){
+        debugger
+        var firstPageUrl = 'https://api.github.com/repos/'+ this.state.searchText +'/issues?page=1&per_page=10';
+        var url = urlValue? urlValue : firstPageUrl;
+
+        // Empty issueList
+        var emptyList = [];
+        this.setState({ issueList : emptyList });
+        // Set error message
+        this.setState({ loading : true });
+        this.setState({ loadingText : 'Loading ...' });
+
+        // set loading text
+        axios.get(url)
             .then(res => {
-              const issues = res.data;
-              // empty loading text
-              this.setState({ loading : false });
-              this.setState({ issueList : issues });
+                debugger
+                const issues = res.data;
+                // empty loading text
+                this.setState({
+                    loading : false,
+                    issueList : issues
+                });
+                var links = res.headers.link.split(',')
+                var linkLength = links.length
+                for(var i = 0; i < linkLength; i++){
+                    var data = links[i].match(/<(.+)>; rel="(.+)"/);
+                    if(data[2] === 'next'){
+                        this.setState({ nextUrl : data[1] })
+                    }
+                    if(data[2] === 'last'){
+                        this.setState({ lastUrl : data[1] })
+                    }
+                    if(data[2] === 'prev'){
+                        this.setState({ prevUrl : data[1] })
+                    }
+                    if(data[2] === 'first'){
+                        this.setState({ firstUrl : data[1] })
+                    }
+                }
 
-              // get buttons 
-              // first
-              // last
-              // next  
-              // prev
+                console.log('Reached here ');
 
-          }).catch(function (error) {
-              // TODO: Handle error here (low priority)
-              // Set error message 
-              this.setState({ loading : false });
-                
-              this.setState({ loadingText : 'Error in fetching data, please check the repository name' });
-          
-              console.log(error);
-          });
-      }
+            }).catch(function (error) {
+            // TODO: Handle error here (low priority)
+            // Set error message
+            this.setState({ loading : false });
 
-      _keyExtractor = (item, index) => item.id;
+            this.setState({ loadingText : 'Error in fetching data, please check the repository name' });
 
-      render() {
-              
-              let loader;
-              let next;
-              let previous;
-              
-              if(this.state.loading){
-                  loader = <Text style={styles.item}>{this.state.loadingText}</Text>
-              }
-              
-          return (
+            console.log(error);
+        });
+    }
+    next(){
 
-              <View style={styles.container}>
-                  
-                  <TextInput 
-                      style={{height: 40, borderColor: 'gray', width:300, borderWidth: 1, backgroundColor: '#fff'}}
-                      placeholderTextColor="white"
-                      onChangeText={(text) => this.setState({searchText:text})}
-                  />
+        this.onPressLearnMore(null,this.state.nextUrl);
+    }
+    previous(){
+        this.onPressLearnMore(null,this.state.prevUrl);
+    }
+    first(){
+        this.onPressLearnMore(null,this.state.firstUrl);
+    }
+    last(){
+        this.onPressLearnMore(null,this.state.lastUrl);
+    }
 
-                  <Button
-                      onPress={this.onPressLearnMore}
-                      title="Get Issuess"
-                      color="#841584"
-                      accessibilityLabel="Learn more about this purple button" />
-              
-                 {loader}       
-                 <FlatList
+    _keyExtractor = (item, index) => item.id;
+
+    render() {
+
+        let loader;
+
+        let next =  <Button
+            onPress={this.next}
+            title="Next"
+            color="#841584"
+            accessibilityLabel="" />;
+        let previous =  <Button
+            onPress={this.previous}
+            title="Prev"
+            color="#841584"
+            accessibilityLabel="" />;
+        let first =  <Button
+            onPress={this.first}
+            title="First"
+            color="#841584"
+            accessibilityLabel="" />;
+        let last =  <Button
+            onPress={this.last}
+            title="Last"
+            color="#841584"
+            accessibilityLabel="" />;
+
+        if(this.state.loading){
+            loader = <Text style={styles.item}>{this.state.loadingText}</Text>
+        }
+
+        return (
+
+            <View style={styles.container}>
+
+                <TextInput
+                    style={{height: 40, borderColor: 'gray', width:300, borderWidth: 1, backgroundColor: '#fff'}}
+                    placeholderTextColor="white"
+                    onChangeText={(text) => this.setState({searchText:text})}
+                />
+
+                <Button
+                    onPress={this.onPressLearnMore}
+                    title="Get Issuess"
+                    color="#841584"
+                    accessibilityLabel="Learn more about this purple button" />
+
+                {loader}
+
+                <FlatList
                     data={this.state.issueList}
                     keyExtractor={this._keyExtractor}
-                    renderItem={({item}) => 
-                          <View style={styles.itemBox}>
+                    renderItem={({item}) =>
+                        <View style={styles.itemBox}>
                             <Text style={styles.item}>Id - {item.id}</Text>
                             <Text style={styles.item}>Title - {item.title}</Text>
-                          </View>                   
-                  }
+                        </View>
+                    }
                 />
-              </View>
-          );
-      }
-  }
 
-  const styles = StyleSheet.create({
-      
-      container: {
-          flex: 1,
-          alignItems: 'center',
-          justifyContent: 'center',
-          marginTop: 30,
-          backgroundColor: '#442358'
-      },
-      item: {
+                {this.state.firstUrl.length > 0 && first}
+                {this.state.prevUrl.length > 0 && previous}
+                {this.state.nextUrl.length > 0 && next}
+                {this.state.lastUrl.length > 0 && last}
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 30,
+        backgroundColor: '#442358'
+    },
+    item: {
         padding: 10,
         fontSize: 18,
         color: '#fff',
         padding: 5,
         margin: 2
-      },
-      itemBox: {
+    },
+    itemBox: {
         borderWidth: 1,
         borderColor: '#fff',
-      }
-  });
+    }
+});
 
-  Expo.registerRootComponent(App);
+Expo.registerRootComponent(App);
